f24a12ccbff0656036ebd4a5e4fab49298ef597b
David Rojo <davidrojo1997@gmail.com>
2021-08-20 17:32:18 +0200

    header done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ContentComponent } from './components/content/content.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
+import { ContentComponent } from './components/content/content.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { HttpClientModule } from '@angular/common/http';
+import { HeadComponent } from './components/head/head.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    HeadComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
