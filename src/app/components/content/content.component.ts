092741b92961539dfc87fb5ee6af105ca7864dd0
David Rojo <davidrojo1997@gmail.com>
2021-08-21 16:40:17 +0200

    75% performance improvement

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -14,7 +14,7 @@ export class ContentComponent implements OnInit {
   dataSize = 25
   dataSizes = [25, 50, 75, 100]
   POKEMONS: Pokemon[] = []
-  API_URL: string = 'https://pokeapi.co/api/v2/pokemon?limit=200'
+  API_URL: string = 'https://pokeapi.co/api/v2/pokemon?limit=100'
   constructor(public pokeService: PokemonService) {}
 
   ngOnInit(): void {
@@ -60,8 +60,14 @@ export class ContentComponent implements OnInit {
   onSizeChange(event: any): void {
     this.dataSize = event.target.value
     this.page = 1
+    const lastPage = 100 / this.dataSize
+
     this.POKEMONS = []
-    this.API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=200'
+    this.API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=100'
     this.fetchData()
+
+    if (this.page === lastPage) {
+      this.fetchData()
+    }
   }
 }
